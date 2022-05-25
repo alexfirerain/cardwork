9918bd333dbdb55b01022774dc291da3dd9f512c
Alexander Lukyanov <lux-nova@mail.ru>
2022-05-25 21:51:20 +0300

    stub #2

---
+++ src/main/java/ru/netology/cardwork/controller/TransferController.java
@@ -0,0 +1,12 @@
+package ru.netology.cardwork.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransferController {
+
+//    public ResponseEntity<String>
+
+}
--- src/main/java/ru/netology/cardwork/model/Card.java
+++ src/main/java/ru/netology/cardwork/model/Card.java
@@ -1,10 +1,33 @@
 package ru.netology.cardwork.model;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class Card {
-    public Card(String cardNumber, String ValidTill, String cardCVV) {
-    }
+    @NotBlank
+    @Pattern(regexp = "[0-9]{16}")
+    private String cardNumber;
 
-    public Card(String cardNumber) {
+    @NotBlank
+    @Pattern(regexp = "[0-9]{4}")
+    @EqualsAndHashCode.Exclude
+    private String ValidTill;
 
-    }
+    @NotBlank
+    @Size(min = 3)
+    @EqualsAndHashCode.Exclude
+    private String cardCVV;
+
+
 }
