2a4003ab962354d73ee69fcbd57e5cff24162c33
Alexander Lukyanov <lux-nova@mail.ru>
2022-08-14 21:24:23 +0300

    v.0.5.2 exceptions' descriptions

--- src/main/java/ru/netology/cardwork/controller/ExceptionController.java
+++ src/main/java/ru/netology/cardwork/controller/ExceptionController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.netology.cardwork.dto.ErrorResponseDto;
 import ru.netology.cardwork.exception.CardNotFoundException;
-import ru.netology.cardwork.exception.FundsInsufficientException;
 import ru.netology.cardwork.exception.TransferNotPossibleException;
 import ru.netology.cardwork.exception.VerificationFailureException;
 
@@ -26,8 +25,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ExceptionController {
     private static final AtomicInteger idCount = new AtomicInteger();
 
+    /**
+     * Handles situations when something is not ok with data in request:
+     * constrains on fields are broken or requierd fields missing,
+     * when any of cards involved is absent or data of donor card not valid,
+     * also when the received verification code is not correct.
+     * @param re an exception being caught.
+     * @return  a response entity with status code 400 and an ErrorResponseDto as body.
+     */
     @ExceptionHandler({VerificationFailureException.class,
-                        FundsInsufficientException.class,
                         CardNotFoundException.class,
                         MethodArgumentNotValidException.class,
                         HttpMessageNotReadableException.class,
@@ -35,18 +41,31 @@ public class ExceptionController {
     ResponseEntity<ErrorResponseDto> handleBadRequest(RuntimeException re) {
         log.debug("Caught an exception: {}", re.getClass());
         log.info("Transfer attempt rejected because of inappropriate request: {}", re.getLocalizedMessage());
-        return new ResponseEntity<>(new ErrorResponseDto("Error in the request: " + re.getMessage(),
+        return new ResponseEntity<>(new ErrorResponseDto("Ошибка в запросе: " + re.getMessage(),
                 idCount.getAndIncrement()), HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Handles situations when the request is ok but the transfer is not possible for some reason:
+     * when any of cards involved is inactive or doesn't have a proper currency account, or when funds at donor account
+     * are insufficient.
+     * @param tnpe an exceptioon being caught.
+     * @return  a response entity with status code 500 and an ErrorResponseDto as body.
+     */
     @ExceptionHandler(TransferNotPossibleException.class)
     ResponseEntity<ErrorResponseDto> handleTransferError(TransferNotPossibleException tnpe) {
         log.debug("Caught an exception: {}", tnpe.getClass());
         log.info("Transfer attempt rejected because of error: {}", tnpe.getLocalizedMessage());
-        return new ResponseEntity<>(new ErrorResponseDto("Committing the transfer not possible: " + tnpe.getMessage(),
+        return new ResponseEntity<>(new ErrorResponseDto("Совершение перевода невозможно: " + tnpe.getMessage(),
                                                 idCount.getAndIncrement()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    /**
+     * Handles all other exceptional cases that are likely during server's exploitation.
+     * Particulary a situation when a verification code comes for the operation whose id is not at the wait list somehow.
+     * @param se an exception being caught.
+     * @return a response entity with status code 500 and an ErrorResponseDto as body.
+     */
     @ExceptionHandler(Exception.class)
     ResponseEntity<ErrorResponseDto> handleServerError(Exception se) {
         log.debug("Caught an exception: {}", se.getClass());
