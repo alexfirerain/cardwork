ac240a4a221d1a57a27c5d9d80b10c67aa150acd
Alexander Lukyanov <lux-nova@mail.ru>
2022-11-10 22:13:11 +0300

    v.1.3e

--- src/test/java/ru/netology/cardwork/ControllerTest.java
+++ src/test/java/ru/netology/cardwork/ControllerTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RequestTest {
+public class ControllerTest {
     @Autowired
     private TransferController transferController;
     @Test
